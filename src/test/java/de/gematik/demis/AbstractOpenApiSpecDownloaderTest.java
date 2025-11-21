33c9f45aae1e87dab231ccdc29bbb945b6eb279a
gematik-Entwicklung <software-development@gematik.de>
2025-11-21 15:47:37 +0100

    Release 2.14.3

--- src/test/java/de/gematik/demis/AbstractOpenApiSpecDownloaderTest.java
+++ src/test/java/de/gematik/demis/AbstractOpenApiSpecDownloaderTest.java
@@ -9,20 +9,21 @@ package de.gematik.demis;
  * Licensed under the EUPL, Version 1.2 or - as soon they will be approved by the
  * European Commission – subsequent versions of the EUPL (the "Licence").
  * You may not use this work except in compliance with the Licence.
- *
+ * 
  * You find a copy of the Licence in the "Licence" file or at
  * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
- *
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
  * In case of changes by gematik find details in the "Readme" file.
- *
+ * 
  * See the Licence for the specific language governing permissions and limitations under the Licence.
- *
+ * 
  * *******
- *
- * For additional notes and disclaimer from gematik and in case of changes by gematik find details in the "Readme" file.
+ * 
+ * For additional notes and disclaimer from gematik and in case of changes by gematik,
+ * find details in the "Readme" file.
  * #L%
  */
 
@@ -37,8 +38,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
